9bfdbbb8b66bafcdb450f2be437f2e80aff22325
Lucas <pro@lucasmercier.com>
2022-03-06 15:12:43 +0100

    Fix cors origin on frontend

--- src/main/java/com/lucasmercier/superhero/feature/incident/controller/IncidentTypesController.java
+++ src/main/java/com/lucasmercier/superhero/feature/incident/controller/IncidentTypesController.java
@@ -4,8 +4,8 @@ import com.lucasmercier.superhero.entity.IncidentType;
 import com.lucasmercier.superhero.feature.incident.contract.IncidentTypeServiceContract;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +13,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/incident-types")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 public class IncidentTypesController {
 
     private final IncidentTypeServiceContract incidentTypeService;
-    
+
     @GetMapping
     public ResponseEntity<List<IncidentType>> getIncidentTypes() {
         return incidentTypeService.getIncidentTypes();
